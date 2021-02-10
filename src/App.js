e9ca9d57770dce0f313b0118dbce5f94c7c4d186
themindfulcreator <themindfulcreator@gmail.com>
2021-02-10 23:45:42 +0800

    Update App.js
    Updated App.js with initial message work in progress.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, stay tuned for my personal website. Work in progress.
         </p>
         <a
           className="App-link"
